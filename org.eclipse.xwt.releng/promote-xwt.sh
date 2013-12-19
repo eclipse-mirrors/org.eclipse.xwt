0f2fffae0c4ff05cf4ffce2da9b8eeddd9b5bf22
Erdal Karaca <erdal.karaca.de@googlemail.com>
2013-12-19 19:31:00 +0100

    Added Missing Update Site URLs

--- org.eclipse.xwt.releng/promote-xwt.sh
+++ org.eclipse.xwt.releng/promote-xwt.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+target=release-0.10.0
 directory="`date +%d-%m-%y-%H-%M`"
-mv /home/data/httpd/download.eclipse.org/xwt/updates /home/data/httpd/archive.eclipse.org/xwt/updates/$directory
+mv /home/data/httpd/download.eclipse.org/xwt/updates /home/data/httpd/archive.eclipse.org/xwt/$directory
 
 rm /home/data/users/ekaraca/promote/temp/site.p2.zip
 rm -rf /home/data/users/ekaraca/promote/temp/site.p2
@@ -12,6 +14,36 @@ wget --no-check-certificate "https://hudson.eclipse.org/hudson/job/xwt-1.0-keple
 #...
 
 unzip site.p2.zip
-rm -rf /home/data/httpd/download.eclipse.org/xwt/updates/
-mkdir /home/data/httpd/download.eclipse.org/xwt/updates/
-cp -r /home/data/users/ekaraca/promote/temp/site.p2/* /home/data/httpd/download.eclipse.org/xwt/updates/
+rm -rf /home/data/httpd/download.eclipse.org/xwt/$target/
+mkdir /home/data/httpd/download.eclipse.org/xwt/$target/
+cp -r /home/data/users/ekaraca/promote/temp/site.p2/* /home/data/httpd/download.eclipse.org/xwt/$target/
+cp site.p2.zip /home/data/httpd/download.eclipse.org/xwt/$target/xwt-archived-p2-site.zip
+
+echo '
+<!DOCTYPE html>
+<html>
+<head>
+  <title>XWT Update Site (Incubation)</title>
+</head>
+
+<body style="font-family:Arial; font-size:small;">
+<h1>XWT Update Site (Incubation)</h1>
+<p>
+<em>For information about XWT, visit its <a href="http://www.eclipse.org/xwt">homepage</a> or <a
+href="http://wiki.eclipse.org/XWT">wiki</a>.
+         <br> For information about installing or updating Eclipse software, see the
+         <a href="http://help.eclipse.org/helios/index.jsp?topic=/org.eclipse.platform.doc.user/tasks/tasks-124.htm">
+           Eclipse Platform Help</a>.
+</p>
+<h3>Contents</h3>
+<ul>
+<li><a href="#">...</a></li>
+</ul>
+<p>
+<a href="xwt-archived-p2-site.zip">Download the zipped XWT p2 site</a>
+</p>
+</body>
+</html>
+' > /home/data/httpd/download.eclipse.org/xwt/$target/index.html
+
+cp -ar /home/data/httpd/download.eclipse.org/xwt/$target /home/data/httpd/download.eclipse.org/xwt/updates
