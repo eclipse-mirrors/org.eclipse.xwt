0d8934f1dfdffd270401cdfb68031bda02c4edb6
Quentin Le Menez <quentin.lemenez@cea.fr>
2019-06-03 10:50:11 +0200

    Bug 547225 - [Releng] Update publish script
    - Add a new publish script to be used in the Jenkins job
    - Archive the old publish script
    
    Change-Id: I4cb5ccb23b95697c621353856112c628b9462801
    Signed-off-by: Quentin Le Menez <quentin.lemenez@cea.fr>

--- org.eclipse.xwt.releng/promote-xwt.sh
+++ org.eclipse.xwt.releng/promote-xwt.sh
@@ -1,24 +1,58 @@
-#!/bin/bash
-target=release-0.10.0
-directory="`date +%d-%m-%y-%H-%M`"
-mv /home/data/httpd/download.eclipse.org/xwt/updates /home/data/httpd/archive.eclipse.org/xwt/$directory
+#!/bin/bash -x
 
-rm /home/data/users/ekaraca/promote/temp/site.p2.zip
-rm -rf /home/data/users/ekaraca/promote/temp/site.p2
+#The specific localization
+remoteRoot="/home/data/httpd/download.eclipse.org"
+xwtRoot="xwt"
 
-cd /home/data/users/ekaraca/promote/temp
-wget --no-check-certificate "https://hudson.eclipse.org/hudson/job/xwt-1.0-kepler/lastSuccessfulBuild/artifact/site.p2.zip"
 
-#...
-# sign our zip
-#...
+########### Parameters Initialization ###########
+if [[ "$MILESTONE" == "-R" ]] ; then
+  destination=$remoteRoot/$xwtRoot/release-$RELEASE_NUMBER
+  destinationUpdateSite=$destination
+else
+  destination=$remoteRoot/$xwtRoot/milestones-$RELEASE_NUMBER
+  destinationUpdateSite=$destination/$RELEASE_NUMBER$MILESTONE
+fi
+#echo $destinationUpdateSite
 
-unzip site.p2.zip
-rm -rf /home/data/httpd/download.eclipse.org/xwt/$target/
-mkdir /home/data/httpd/download.eclipse.org/xwt/$target/
-cp -r /home/data/users/ekaraca/promote/temp/site.p2/* /home/data/httpd/download.eclipse.org/xwt/$target/
-cp site.p2.zip /home/data/httpd/download.eclipse.org/xwt/$target/xwt-archived-p2-site.zip
+jobArtifacts=$HOME/.jenkins/jobs/$PROMOTED_JOB_NAME/builds/$PROMOTED_NUMBER/archive
+#echo $jobArtifacts
+#ls $jobArtifacts
 
+if $SIGN ; then
+  :
+else
+  echo "The sources may not have been signed. Please verify the build and artifacts parameters and try again."
+  exit 1
+fi
+
+if [ -d $destination ] ; then
+  if [ -d $destinationUpdateSite ] ; then
+    if $OVERRIDE ; then
+      echo "Removing previous artifacts"
+      rm -rf $destinationUpdateSite
+    else
+      echo "The milestone already exists. You might want to change the suffix or delete the previous one."
+      exit 1
+    fi
+  fi
+fi
+
+
+########### Publish Artifacts ###########
+#Go to the artifact directory
+cd $jobArtifacts
+echo "Promoting the Job to $destinationUpdateSite"
+#No error if exists, makes parent directories as needed
+mkdir -p $destinationUpdateSite
+
+#Copy the contents onto the server folder
+cp -a *.zip $destinationUpdateSite
+cp -a repository/* $destinationUpdateSite
+
+
+########### Create Index File ###########
+function createIndexFile() {
 echo '
 <!DOCTYPE html>
 <html>
@@ -44,6 +78,20 @@ href="http://wiki.eclipse.org/XWT">wiki</a>.
 </p>
 </body>
 </html>
-' > /home/data/httpd/download.eclipse.org/xwt/$target/index.html
+' > $1/index.html
+}
+echo "Create index.html file for the new repository"
+cerateIndexFile $destinationUpdateSite
 
-cp -ar /home/data/httpd/download.eclipse.org/xwt/$target /home/data/httpd/download.eclipse.org/xwt/updates
+
+########### Set Access Rights ###########
+# This function sets the acess rights to allow all memebers of the group to edit the files
+function setAccessRights() {
+	chmod -R 775 "$1"
+	chgrp -hR technology.xwt "$1"
+}
+echo "Set access right -R: $destinationUpdateSite"
+setAccessRights $destinationUpdateSite
+
+
+echo "publishing done."
